9eb728c30a1986ab0ae2dd5a54c09c72bb6dd794
Sebastien Lago <seblago@gmail.com>
2015-02-09 22:20:22 -0500

    Creation des classes film, personneCineme, personnageFilm, genreFilm, filmPays et scenariste.

--- lab-hibernate/src/Main.java
+++ lab-hibernate/src/Main.java
@@ -1,41 +1,110 @@
 
-import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
-
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class Main {
 	public static void main(String[] args) {
-
-		Session sessionHome = HibernateUtil.getSessionFactory().openSession();
-		Transaction transaction = null;
+		Session uneSession = HibernateUtil.getSessionFactory().openSession();
 		try {
-			transaction = sessionHome.beginTransaction();
-			Client client1 = new Client();
-			Client client2 = new Client();
+			uneSession = HibernateUtil.getSessionFactory().openSession(); 
+			 uneSession.beginTransaction(); 
+			 List lesFilms = uneSession.createQuery( 
+			 "FROM Film f " + 
+			 "WHERE f.titre = 'Twins' OR f.titre = 'Monsters vs Aliens' " +
+			 "ORDER BY f.titre ASC").list(); 
+			 
+			 System.out.println( lesFilms.size() + " films trouvés:" ); 
+			 
+			 for ( Iterator iterFilms = lesFilms.iterator(); iterFilms.hasNext(); ) { 
+				 Film unFilmCharge = (Film) iterFilms.next(); 
+				 //System.out.println("Film:" + unFilmCharge.getTitre() + " Realisateur: " + unFilmCharge.getRealisateur().getNom()); 
+				 //System.out.println("Annee:" + unFilmCharge.getAnnee()); 
+				 //System.out.println("Langue:" + unFilmCharge.getLangue());
+				 //System.out.println("Duree:" + unFilmCharge.getDuree()); 
+				 //System.out.println("Resume:" + unFilmCharge.getResume()); 
+				 
+				 //displayGenres(unFilmCharge.getGenres());
+				 //displayPays(unFilmCharge.getPays());
+				 //displayScenaristes(unFilmCharge.getScenaristes());
+				 //displayPersonnages(unFilmCharge.getPersonnages());
+				 displayActeurs(unFilmCharge.getPersonnages());
+				 
+				 /*System.out.println("Genres de film(" + unFilmCharge.getGenres().size() + "):" ); 
+			 
+				 for ( Iterator iterGenres = unFilmCharge.getGenres().iterator(); iterGenres.hasNext(); ) { 
+					 GenreFilm unGenreCharge = (GenreFilm) iterGenres.next(); 
+					 System.out.println(" " + unGenreCharge.getNomGenre() ); 
+				 } */
+			 } 
+			 uneSession.getTransaction().commit(); 
+			 uneSession.close(); 
+			 System.exit(0);
 			
-			/*Set<Course> courses = new HashSet<Course>();
-			courses.add(course1);
-			courses.add(course2);*/
-
-			/*Student student1 = new Student(1, "Eswar", courses);
-			Student student2 = new Student(2, "Joe", courses);
-			sessionHome.save(course1);
-			sessionHome.save(course2);
-			sessionHome.save(student1);
-			sessionHome.save(student2);*/
-
-			transaction.commit();
 		} catch (HibernateException e) {
-			transaction.rollback();
 			e.printStackTrace();
+			System.exit(1);
 		} finally {
-			sessionHome.close();
+			if(uneSession.isOpen())
+				uneSession.close();
 		}
-
+	}
+	
+	public static void displayGenres(Set genres){
+		System.out.println("Genres de film(" + genres.size() + "):" ); 
+		 
+		 for ( Iterator iterGenres = genres.iterator(); iterGenres.hasNext(); ) { 
+			 GenreFilm unGenreCharge = (GenreFilm) iterGenres.next(); 
+			 System.out.println(" " + unGenreCharge.getNomGenre() ); 
+		 }
+	}
+	
+	public static void displayPays(Set pays){
+		System.out.println("Pays de film(" + pays.size() + "):" ); 
+		 
+		 for ( Iterator iterPays = pays.iterator(); iterPays.hasNext(); ) { 
+			 FilmPays unPaysCharge = (FilmPays) iterPays.next(); 
+			 System.out.println(" " + unPaysCharge.getNomPays() ); 
+		 }
+	}
+	
+	public static void displayScenaristes(Set scenaristes){
+		System.out.println("Scenaristes de film(" + scenaristes.size() + "):" ); 
+		 
+		 for ( Iterator iterScenariste = scenaristes.iterator(); iterScenariste.hasNext(); ) { 
+			 Scenariste unPaysCharge = (Scenariste) iterScenariste.next(); 
+			 System.out.println(" " + unPaysCharge.getNomScenariste() ); 
+		 }
+	}
+	
+	public static void displayPersonnages(Set personnages){
+		System.out.println("Personnages de film(" + personnages.size() + "):" ); 
+		 
+		 for ( Iterator iterPersonnage = personnages.iterator(); iterPersonnage.hasNext(); ) { 
+			 PersonnageFilm unPersonnageCharge = (PersonnageFilm) iterPersonnage.next(); 
+			 System.out.println("pers: " + unPersonnageCharge.getNomPersonnage() + " acteur: " + unPersonnageCharge.getPersonneCinema().getNom()); 
+		 }
+	}
+	
+	public static void displayActeurs(Set personnages){
+		System.out.println("Personnages de film(" + personnages.size() + "):" ); 
+		
+		
+		 for ( Iterator iterPersonnage = personnages.iterator(); iterPersonnage.hasNext(); ) { 
+			 
+			 PersonnageFilm unPersonnageCharge = (PersonnageFilm) iterPersonnage.next();
+			 
+			 PersonneCinema acteur =  unPersonnageCharge.getPersonneCinema();
+			 
+			 System.out.println("pers: " + unPersonnageCharge.getNomPersonnage() + " acteur: " + acteur.getNom());
+			 System.out.println("Lieu naissance: " + acteur.getLieuNaissance()); 
+			 System.out.println("Date naissance: " + acteur.getDateNaissance().toString());
+			 System.out.println("Biographie: " + acteur.getBiographie());
+		 }
 	}
 }
 
---
+++ lab-hibernate/src/Scenariste.java
@@ -0,0 +1,43 @@
+// default package
+// Created by Sebastien Lago
+
+/**
+ * GenreFilm
+ */
+public class Scenariste implements java.io.Serializable {
+	
+	private String idScenaristeFilm;
+	private String nomScenariste;
+	private Film film;
+	
+	public Scenariste() { }
+
+	public Scenariste(String idScenaristeFilm, String nomScenariste) {
+		this.idScenaristeFilm = idScenaristeFilm;
+		this.nomScenariste = nomScenariste;
+	}
+	
+	public String getIdScenaristeFilm() {
+		return this.idScenaristeFilm;
+	}
+
+	public void setIdScenaristeFilm(String idScenaristeFilm) {
+		this.idScenaristeFilm = idScenaristeFilm;
+	}
+	
+	public String getNomScenariste() {
+		return this.nomScenariste;
+	}
+
+	public void setNomScenariste(String nomScenariste) {
+		this.nomScenariste = nomScenariste;
+	}
+	
+	public Film getFilm() {
+		return this.film;
+	}
+	
+	public void setFilm(Film film) {
+		this.film = film;
+	}
+}
